e4b18e08b4b05069a0f7fa52aa44d3be4743c396
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-12-08 17:18:41 +0100

    Change cv and add validation for form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -15,7 +15,7 @@ import './contact.scss';
 const Contact = () => {
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -28,7 +28,9 @@ const Contact = () => {
 
   const sendEmail = (event) => {
     event.preventDefault();
-    emailjs.sendForm('service_00rzze1', 'template_ygvnq4o', event.target, 'user_KIqGbQLwhpWks7FD3tx7z')
+    handleSubmit(event);
+    if (validated === true) {
+      emailjs.sendForm('service_00rzze1', 'template_ygvnq4o', event.target, 'user_KIqGbQLwhpWks7FD3tx7z')
       .then((result) => {
         console.log(result.text);
         if (result.text === 'OK') {
@@ -41,6 +43,11 @@ const Contact = () => {
       event.target.reset();
       grecaptcha.reset();
       setVerified(false);
+      setValidated(false);
+    } else {
+      handleSubmit(event);
+    } 
+    
   };
 
   const [verified, setVerified] = useState(false);
@@ -55,7 +62,7 @@ const Contact = () => {
       id="contact"
       noValidate
       validated={validated}
-      onSubmit={handleSubmit, sendEmail}
+      onSubmit={sendEmail}
       sm="auto"
       md="auto"
       lg="auto"
@@ -67,23 +74,25 @@ const Contact = () => {
         Vous pouvez me contacter en remplissant le formulaire ci-dessous. Je vous répondrai dans les meilleurs délais.
       </div>
       <Row>
-        <div className="name">
-        <Form.Group
-          as={Col}
-          sm="auto"
-          md="auto"
-          lg="auto"
-          controlId="validationLastName"
-        >
-          <Form.Label>Nom</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Votre nom"
-            name="lastname"
-          />
-          <Form.Control.Feedback>Nom OK</Form.Control.Feedback>
-        </Form.Group>
+        <div className="lastname">
+          <Form.Group
+            as={Col}
+            sm="auto"
+            md="auto"
+            lg="auto"
+            controlId="validationLastName"
+          >
+            <Form.Label>Nom</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Votre nom"
+              name="lastname"
+            />
+            <Form.Control.Feedback type="invalid">Veuillez saisir votre nom.</Form.Control.Feedback>
+          </Form.Group>
+         </div>
+         <div className="firstname">
         <Form.Group
           as={Col}
           sm="auto"
@@ -98,7 +107,7 @@ const Contact = () => {
             placeholder="Votre prénom"
             name="firstname"
           />
-          <Form.Control.Feedback>Prénom OK</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">Veuillez saisir votre prénom.</Form.Control.Feedback>
         </Form.Group>
         </div>
         <div className="mail">
@@ -116,7 +125,7 @@ const Contact = () => {
             placeholder="Votre email"
             name="email"
           />
-          <Form.Control.Feedback>Email OK</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">Veuillez saisir un email valide.</Form.Control.Feedback>
         </Form.Group>
         </div>
         <div className="message">
@@ -136,7 +145,7 @@ const Contact = () => {
             name="message"
             style={{ height: '180px' }}
           />
-          <Form.Control.Feedback>Message OK</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">Veuillez saisir votre message.</Form.Control.Feedback>
         </Form.Group>
         </div>
         
